af4fd0868c629951906c1c47ce2cff4f69ff253d
Leonardo López <leonard_156@hotmail.com>
2017-01-04 22:11:40 -0600

    Updated to ember version 2.10. SUPPORT for named and no named arguments. DOCUMENTATION UPDATED.

--- README.md
+++ README.md
@@ -8,32 +8,67 @@ This README outlines the details of collaborating on this Ember addon.
 * `npm install`
 * `bower install`
 
-## Example
+## Example with arguments
 
 * `{{format-currency 1000}}`
 
-$ 1,000.00
+$ 1, 000.00
 
 * `{{format-currency 1000.30}}`
 
-$ 1,000.30
+$ 1, 000.30
 
 * `{{format-currency 900.30 2}}`
 
-900.30
+$ 900.30
 
-* `{{format-currency 900.30 3 "€"}}`
+* `{{format-currency 900.303 3 "€"}}`
 
-€ 900.300
+€ 900.303
 
-* `{{format-currency 4300.30 3 "$" " "}}`
+* `{{format-currency 4300.303 3 "$" " "}}`
 
-$ 4 300.300
+$ 4 300.303
 
 * `{{format-currency 4300.30 3 "$" ","}}`
 
 $ 4,300.300
 
+## Example with named arguments
+
+* `{{format-currency 1000}}`
+
+$ 1, 000.00
+
+* `{{format-currency 1000.30}}`
+
+$ 1, 000.30
+
+* `{{format-currency 900.30 decimals=2}}`
+
+$ 900.30
+
+* `{{format-currency 900.303 decimals=3 sign="€"}}`
+
+€ 900.303
+
+* `{{format-currency 4300.303 decimals=3 sign="$" separator=" "}}`
+
+$ 4 300.303
+
+* `{{format-currency 4300.30 decimals=3 sign="$" separator=","}}`
+
+$ 4,300.300
+
+
+## Arguments and named arguments
+
+You can mix both, no named arguments and named arguments,
+BUT IT IS IMPORTAN THE ORDER WHEN YOU USE NO NAMED ARGUMENTS
+the order of the no named arguments
+First argument: decimals
+Second argument: sign
+Third argument: thousands separator
 ## Running
 
 * `ember server`
--- addon/helpers/format-currency.js
+++ addon/helpers/format-currency.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 
-export function formatCurrency([quantity,decimals,symbol,separator]) {
-    if (!symbol){ symbol = '$'; }
-    if (!decimals){ decimals = 2; }
-    if (!separator){ separator = ","; }
-   
-    var amount = parseFloat(quantity);
-    var amount_rounded = amount.toFixed(decimals);
-    var decimal_part = amount_rounded.toString().split('.')[1];
-    var number_part = amount_rounded.toString().split('.')[0];
-    var number = number_part.replace(/\B(?=(\d{3})+(?!\d))/g, separator);
-    return symbol.toString() + " " + number + "." + decimal_part;
+export function formatCurrency([quantity, decimals, sign, separator], namedArgs) {
+  sign = sign !== undefined ? sign : namedArgs.sign !== undefined ? namedArgs.sign : "$";
+  decimals = decimals !== undefined ? decimals : namedArgs.decimals !== undefined ? namedArgs.decimals : 2;
+  separator = separator !== undefined ? separator : namedArgs.separator !== undefined ? namedArgs.separator : ", ";
+
+  let amount = parseFloat(quantity);
+  let amount_rounded = amount.toFixed(decimals);
+  let decimal_part = amount_rounded.toString().split('.')[1];
+  let number_part = amount_rounded.toString().split('.')[0];
+  let number = number_part.replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+  return sign.toString() + " " + number + "." + decimal_part;
 }
 
 export default Ember.Helper.helper(formatCurrency);
