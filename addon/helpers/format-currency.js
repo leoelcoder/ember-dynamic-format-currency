6b94358ae0b4005aff3c52cdf853cf23caf3e827
Gastón I. Silva <givanse@gmail.com>
2017-12-22 19:51:54 -0800

    minor edits

--- addon/helpers/format-currency.js
+++ addon/helpers/format-currency.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 
 export function formatCurrency([quantity, decimals, sign, separator], namedArgs) {
-  sign = sign !== undefined ? sign : namedArgs.sign !== undefined ? namedArgs.sign : "$";
+  sign = sign !== undefined ? sign : namedArgs.sign !== undefined ? namedArgs.sign : '$';
   decimals = decimals !== undefined ? decimals : namedArgs.decimals !== undefined ? namedArgs.decimals : 2;
-  separator = separator !== undefined ? separator : namedArgs.separator !== undefined ? namedArgs.separator : ", ";
+  separator = separator !== undefined ? separator : namedArgs.separator !== undefined ? namedArgs.separator : ',';
 
-  let amount = parseFloat(quantity);
-  let amount_rounded = amount.toFixed(decimals);
-  let decimal_part = amount_rounded.toString().split('.')[1];
-  let number_part = amount_rounded.toString().split('.')[0];
-  let number = number_part.replace(/\B(?=(\d{3})+(?!\d))/g, separator);
-  return sign.toString() + " " + number + "." + decimal_part;
+  const amount = parseFloat(quantity);
+  const amount_rounded = amount.toFixed(decimals);
+  const decimal_part = amount_rounded.toString().split('.')[1];
+  const number_part = amount_rounded.toString().split('.')[0];
+  const number = number_part.replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+  return sign.toString() + number + '.' + decimal_part;
 }
 
 export default Ember.Helper.helper(formatCurrency);
